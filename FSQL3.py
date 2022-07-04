3f0f782b5f7af0a1fb9b137c9752fcdd6a768d27
N00BM45T3R69 <98582199+N00BM45T3R69@users.noreply.github.com>
2022-07-04 22:08:34 +0530

    Delete FSQL3.py

--- FSQL3.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-con=sqlite3.connect("employee.db")
-if con==True:
-    print('Connected to Database')
-con.execute('create table employees(id integer primary key autoincrement,name text not null,email text not null,address text not null)')
-print('Created Table')
